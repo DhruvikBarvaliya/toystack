2adb0edaa445bb37b6a0df970342c4a423bb428b
DhruvikBarvaliya <dhruvik.barvaliya.blackwolve@gmail.com>
2024-06-25 15:34:12 +0530

    test

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/test", (req, res) => {
+  res.json({ Message: "Test" });
 });
 
 app.listen(port, () => {
