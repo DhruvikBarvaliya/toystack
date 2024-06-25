534b272e8a2fb79c9f00435a7ba645cf4a9d463d
DhruvikBarvaliya <dhruvik.barvaliya.blackwolve@gmail.com>
2024-06-25 15:39:27 +0530

    demo

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 4000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
